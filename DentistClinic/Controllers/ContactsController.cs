e2cfeafb7d98dea6871cab29659375ac5ee21ad6
Osama <osama.el.gedawey@gmail.com>
2024-03-19 05:55:27 +0200

    add messages

--- DentistClinic/Controllers/ContactsController.cs
+++ DentistClinic/Controllers/ContactsController.cs
@@ -1,12 +1,79 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DentistClinic.Core.Models;
+using DentistClinic.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DentistClinic.Controllers
 {
 	public class ContactsController : Controller
 	{
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ContactsController(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+		[Authorize(Roles = "Doctor")]
 		public IActionResult Index()
+		{
+            var model = _unitOfWork.contactRepository.GetAll();
+            return View(model);
+        }
+		[Authorize(Roles = "Doctor")]
+		public IActionResult Confirm(int id)
+		{
+			if (id == 0)
+				return BadRequest();
+			var msg = _unitOfWork.contactRepository.GetById(id);
+			if (msg != null)
+			{
+				msg.IsConfirmed = true;
+				_unitOfWork.contactRepository.Update(msg);
+				return RedirectToAction("Index");
+			}
+			return NotFound();
+		}
+		[Authorize(Roles = "Doctor")]
+		public IActionResult UnConfirm(int id)
+		{
+			if (id == 0)
+				return BadRequest();
+			var msg = _unitOfWork.contactRepository.GetById(id);
+			if (msg != null)
+			{
+				msg.IsConfirmed = false;
+				_unitOfWork.contactRepository.Update(msg);
+				return RedirectToAction("Index");
+			}
+			return NotFound();
+		}
+		[Authorize(Roles = "Doctor")]
+		public IActionResult Delete(int id)
+		{
+			if (id == 0)
+				return BadRequest();
+			var msg = _unitOfWork.contactRepository.GetById(id);
+			if (msg != null)
+			{
+				_unitOfWork.contactRepository.Delete(msg);
+				return RedirectToAction("Index");
+			}
+			return NotFound();
+		}
+		public IActionResult Create()
 		{
 			return View();
 		}
+		[HttpPost]
+		public IActionResult Create(ContactMsg msg)
+		{
+			if (ModelState.IsValid)
+			{
+				_unitOfWork.contactRepository.Create(msg);
+				return RedirectToAction("Index", "Home");
+			}
+			return View(msg);
+		}
+
 	}
 }
--- DentistClinic/Core/Models/ContactMsg.cs
+++ DentistClinic/Core/Models/ContactMsg.cs
@@ -1,12 +1,23 @@
-﻿namespace DentistClinic.Core.Models
+﻿using DentistClinic.Core.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace DentistClinic.Core.Models
 {
 	public class ContactMsg
 	{
 		public int Id { get; set; }
-		public string Name { get; set; } = null!;
-		public string Email { get; set; } = null!; 
-		public string Phone { get; set; } = null!;
-		public string Message { get; set; } = null!;
+        [Required]
+        [MinLength(3, ErrorMessage = "Name must be 3 or more char")]
+        public string Name { get; set; } = null!;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+        [Required]
+        [RegularExpression(@"^(\+201|01|00201)[0-2,5]{1}[0-9]{8}", ErrorMessage = Errors.phoneValidation)]
+        public string Phone { get; set; } = null!;
+        [Required]
+        [MinLength(10, ErrorMessage = "Message must be 10 or more char")]
+        public string Message { get; set; } = null!;
 		public bool IsConfirmed { get; set; }=false;
 
 	}
